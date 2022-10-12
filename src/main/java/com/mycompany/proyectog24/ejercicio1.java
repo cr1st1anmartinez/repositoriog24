5b558e6c79b11e8782a83ff90ff4a45b8c810a45
cristian martinez <cr1st1anmart1nez959@gmail.com>
2022-10-12 10:08:40 -0500

    primer proyecto

---
+++ src/main/java/com/mycompany/proyectog24/ejercicio1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectog24;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ejercicio1 {
+
+    public void metodo() {
+        System.out.println("Logica Metodo");
+
+    }
+}
